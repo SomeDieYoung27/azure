cbcaedbd5349925a1577fc4d1dedc8441b86eadb
Shashwat Chaturvedi <111681395+SomeDieYoung27@users.noreply.github.com>
2025-06-15 16:07:01 +0530

    Feat :Parser

---
+++ app/backend/predocslib/parser.py
@@ -0,0 +1,13 @@
+from abc import ABC
+from collections.abc import AsyncGenerator
+from typing import IO
+from .page import Page
+
+
+class Parser(ABC):
+    """
+    Abstract parser that parses content into Page objects
+    """
+    async def parse(self,content : IO) -> AsyncGenerator[Page,None]:
+        if False:
+            yield
